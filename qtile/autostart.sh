04a80ac91fb14c058750babfb795b8e4bf26c115
Héctor Adán <hectorino2789@gmail.com>
2024-06-11 22:45:17 -0600

    Adding DWM config

--- qtile/autostart.sh
+++ qtile/autostart.sh
@@ -3,6 +3,6 @@
 cbatticon -u 5 &
 volumeicon &
 # Change your wallpaper path here!
-feh --bg-fill /home/hectorio23/Desktop/wallpapers/Art\ digital/Distros.png
+feh --bg-fill /home/hectorio23/Desktop/wallpapers2/0004.jpg
 setxkbmap es
 picom --no-vsync &
